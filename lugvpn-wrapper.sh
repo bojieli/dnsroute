2bc679add73381374950b90ab7ed1dcaa2c5e146
Bojie Li <bojieli@gmail.com>
2015-04-26 15:50:13 +0800

    use nohup to run dnsroute

--- lugvpn-wrapper.sh
+++ lugvpn-wrapper.sh
@@ -3,5 +3,5 @@ cd `dirname $0`
 mkdir -p /var/log/dnsroute
 pkill dnsroute # kill all running instances
 for intf in tun{0..5}; do
-	./dnsroute $intf paper-sites.txt 10007 202.141.176.126 >>/var/log/dnsroute/dnsroute-$intf.log 2>&1 &
+	nohup ./dnsroute $intf paper-sites.txt 10007 202.141.176.126 >>/var/log/dnsroute/dnsroute-$intf.log 2>&1 &
 done
